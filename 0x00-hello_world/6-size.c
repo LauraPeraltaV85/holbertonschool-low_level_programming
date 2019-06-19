d31829c06f03d45362be24a08b0a065c3b1c7b19
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-06-19 22:50:18 +0000

    sizeof.3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ longint lintegerType;
 longlongint llintegerType;
 float floatType;
 
-printf("Size of a char: %1d bytes\n", sizeof(charType));
-printf("Size of an int: %ld bytes\n", sizeof(integerType));
-printf("Size of a long int: %ld bytes\n", sizeof(lintegerType));
-printf("Size of a long long int: %ld bytes\n", sizeof(llintegerType));
-printf("Size of a float: %ld bytes\n", sizeof(floatType));
+printf("Size of a char: %1d byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(lintegerType));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(llintegerType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 return (0);
 }
