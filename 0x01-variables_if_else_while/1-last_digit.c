4096b98c2ef57137ce429ecf6c7c27ad5f3ab2a3
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-06-20 17:29:25 +0000

    spaces fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ ln = n % 10;
 if (ln > 5)
 printf("Last digit of %d is %d is greater than 5\n", n, ln);
 else if (ln == 0)
-printf("Last digit of %d is %d is and  is  0\n", n, ln);
+printf("Last digit of %d is %d is and is 0\n", n, ln);
 else if (ln < 6 && ln != 0)
 printf("Last digit of %d is %d is less than 6 and not 0\n", n, ln);
 return (0);
