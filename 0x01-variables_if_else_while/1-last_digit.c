a8724e29f8c20fe301a48b6d99167f8951d3702a
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-06-20 17:55:39 +0000

    add and

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ n = rand() - RAND_MAX / 2;
 ln = n % 10;
 if (ln > 5)
 {
-printf("Last digit of %d is %d is greater than 5\n", n, ln);
+printf("Last digit of %d is %d and is greater than 5\n", n, ln);
 }
 else if (ln == 0)
 {
@@ -27,7 +27,7 @@ printf("Last digit of %d is %d and is 0\n", n, ln);
 }
 else
 {
-printf("Last digit of %d is %d is less than 6 and not 0\n", n, ln);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ln);
 }
 return (0);
 }
