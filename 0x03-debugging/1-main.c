9f3a1c7ee3090143c80c8accf582252fa7b637b3
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-06-26 04:37:09 +0000

    betty corrections

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/**
- *while (i < 10) - removed as per request
- *{
- *putchar(i);
- *}
-*/
+
+/*while (i < 10) - removed as per request*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
