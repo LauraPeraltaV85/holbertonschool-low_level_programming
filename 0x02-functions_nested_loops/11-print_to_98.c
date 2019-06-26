9414a3505dfce5f1f8e42c0caeca0eb4532351c5
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-06-26 00:45:39 +0000

    upload

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,17 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for ( ; n <= 98; n++)
+for ( ; n < 98; n++)
 {
 printf("%d, ", n);
 }
 }
- else
+else
 {
 for ( ; n > 98; n--)
 {
 printf("%d, ", n);
 }
-printf("%d\n", n);
 }
+printf("%d\n", n);
 }
