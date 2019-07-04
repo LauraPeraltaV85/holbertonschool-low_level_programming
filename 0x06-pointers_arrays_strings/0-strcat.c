0a26ad24a2b00ac34727005a5fec4a7935a88046
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-04 21:01:26 +0000

    betty corrections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int destlen, srclen;
+int destlen, srclen;
 
-  for (destlen = 0; dest[destlen] != '\0'; destlen++)
-    ;
-  for (srclen = 0; src[srclen] != '\0'; srclen++, destlen++)
-    {
-  dest[destlen] = src[srclen];
-    }
-  dest[destlen] = '\0';
-  
-  return (dest);
-} 
+for (destlen = 0; dest[destlen] != '\0'; destlen++)
+;
+for (srclen = 0; src[srclen] != '\0'; srclen++, destlen++)
+{
+dest[destlen] = src[srclen];
+}
+dest[destlen] = '\0';
+return (dest);
+}
