a20827e2bea2ede0de7b887e13afffd4e1008d7b
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-04 23:35:49 +0000

    5 and 6

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *reverse_array - print array in reverse
+ *@a: array of integers
+ *@n: number of elements of the array 
+ *Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+
+
+
+}
