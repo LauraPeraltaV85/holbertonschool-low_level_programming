106ae8f778694d77c8f2609c3990c99323513e93
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-10 21:55:01 +0000

    [fix] betty style errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,39 @@
 #include "holberton.h"
 
 /**
- *_sqrt_recursion - returns natural square root of a number
- *@n: number
+ *squared - returns natural square root of a number
+ *@a: square root
+ *@b: number
  *Return: square root of n, if no square root return -1
  */
 int squared(int a, int b)
 {
-    if (b == a *a)
-    {
-    return (a);
-    }
-  if (a == b)
-    return (-1);
-  return (squared(a + 1, b));
-
+if (b == a * a)
+{
+return (a);
 }
+if (a == b)
+{
+return (-1);
+}
+return (squared(a + 1, b));
+}
+
+/**
+ *_sqrt_recursion - returns natural square root of a number
+ *@n: number
+ *Return: square root of n, if no square root return -1
+ */
+
 int _sqrt_recursion(int n)
 {
-  if (n <= 0)
-    {
-      return (-1);
+if (n <= 0)
+{
+return (-1);
 }
-  if (n == 1)
-    {
-      return (n);
+if (n == 1)
+{
+return (n);
 }
-  return (squared(1, n));
+return (squared(1, n));
 }
