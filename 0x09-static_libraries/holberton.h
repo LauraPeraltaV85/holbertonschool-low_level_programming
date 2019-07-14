086b44ea1ee273810e534be3910c0717eca1bc3a
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-14 16:16:48 +0000

    [fix] delete extra functions listed

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -21,14 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
 
 #endif   
