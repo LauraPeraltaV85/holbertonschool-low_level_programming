ac297d8705a62ff7ec279284c9fa89cafb336696
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-14 17:19:14 +0000

    [feature] prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - prints name of the program
+ *@argc: counter
+ *@argv: array of strings (arguments)
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+printf("%s\n", argv[argc - 1]);
+
+return (0);
+}
