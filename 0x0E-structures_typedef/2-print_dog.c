ca5435ff1661a56114154dd43c2192a3e339ab50
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-23 01:39:29 +0000

    [fix] equal to nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,13 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if(d->name != NULL)
+	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
-	else 
-		printf("Name: (nil)\n");
-        printf("Age: %f\n", d->age);
-	if(d->owner != NULL)
+	else (d->name = "(nil)")
+		     printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
-	else
-		printf("Owner: (nil)\n");
+	else (d->owner = "(nil)")
+		     printf("Owner: %s\n", d->owner);
 }
