593ce951fc76cc6d45891736b2cf20d9e5a0450f
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-25 01:33:05 +0000

    [fix] bettys errors, error messages locations into op_functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,27 +1,24 @@
-#include "calc.h"
+#include "3-calc.h"
 #include "function_pointers.h"
 
 /**
  *main - asks for arguments and prints result
+ *@argc: number of args
+ *@argv: args
  *Return: 0
  */
 int main(int argc, char **argv)
 {
 
 	int e, l, rest;
-	
+
 	if (argc == 4)
 	{
 		e = atoi(argv[1]);
-                l = atoi(argv[3]);
-                rest = get_op_func(argv[2])(e, l);
-                printf("%i\n", rest);
-                return (0);
-	}
-	if (argv[2] == '/' || argv[2] == '%' && argv[3] == 0)
-	{
-		printf("Error\n");
-		exit(100);
+		l = atoi(argv[3]);
+		rest = get_op_func(argv[2])(e, l);
+		printf("%i\n", rest);
+		return (0);
 	}
 	printf("Error\n");
 	exit(98);
