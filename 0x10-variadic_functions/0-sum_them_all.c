ab6aaccf83398042a54db90942e761ad131850a1
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-27 17:39:43 +0000

    [fix] order in va declaration

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,10 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int e;
 	double sum = 0;
 
+	va_start(arglist, n);
+
 	if (n == 0)
 		return (0);
-
-	va_start(arglist, n);
 
 	for (e = 0; e < n; e++)
 	{
