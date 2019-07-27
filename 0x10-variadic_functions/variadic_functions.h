52675b982d8085b494203315cc8353f38e737221
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-07-27 17:32:22 +0000

    [feature] description of project and header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef _VARIADIC_
+#define _VARIADIC_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void _c(va_list);
+void _i(va_list);
+void _f(va_list);
+void _s(va_list);
+
+typedef struct FORM
+{
+	char *k;
+	void (*fc)(va_list);
+} print;
+
+#endif
