0cb33131a864ee5e262976287976f288b019c962
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-02 00:46:04 +0000

    [feature] print before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void first(void)__attribute__((construct));
+/**
+ *first - print string before executing main file
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
