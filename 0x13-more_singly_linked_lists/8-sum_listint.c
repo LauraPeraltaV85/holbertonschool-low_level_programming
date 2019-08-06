4eabb4ed42e64ce842f33c2f01b7ca56580ffdfe
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-06 16:20:13 +0000

    [feature] sums data elements in list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *sum_listint - sums all the data(n) of a listint_t
+ *@head: pointer to list struct
+ *Return: sum of data or 0 if empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
