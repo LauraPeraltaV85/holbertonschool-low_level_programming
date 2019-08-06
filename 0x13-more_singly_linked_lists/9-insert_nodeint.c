e5731092c22ee36894d6f997b068a9d4ee1534a2
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-06 19:39:52 +0000

    [fix] add condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = *head;
 		for (count = 1; count < idx; count++)
+		{
+			if (temp == NULL)
+				return (NULL);
 			temp = temp->next;
+		}
 		newnode->next = temp->next;
 		temp->next = newnode;
 	}
