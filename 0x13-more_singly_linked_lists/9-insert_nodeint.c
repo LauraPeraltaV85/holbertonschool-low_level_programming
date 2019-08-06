d28d5bcacc39c6cdc65203afcb4783d06a7a03d2
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-06 19:23:58 +0000

    [fix] delete condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,12 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = *head;
 		for (count = 1; count < idx; count++)
-		{
-			if (temp->next)
-			{
 			temp = temp->next;
-			}
-		}
 		newnode->next = temp->next;
 		temp->next = newnode;
 	}
