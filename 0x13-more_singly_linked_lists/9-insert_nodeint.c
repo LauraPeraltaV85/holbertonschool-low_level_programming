50a39d3ebc91e957d2041f873cf49a6b53884ee2
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-06 18:11:52 +0000

    [fix] modify new condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,8 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode->n = n;
 	if (head == NULL && *head == NULL)
-/**head = newnode;*/
-		return(NULL);
+		*head = newnode;
 	else
 	{
 		temp = *head;
