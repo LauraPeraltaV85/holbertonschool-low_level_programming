f89ebf0540244dfcc4a9f93a41a7b7df94abb218
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-06 20:04:37 +0000

    [fix] modify condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,13 +12,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newnode, *temp;
 	unsigned int count;
 
+	if (head == NULL)
+		return (NULL);
+
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 
 	newnode->n = n;
 	if (idx == 0)
+	{
+		newnode->next = *head;
 		*head = newnode;
+	}
 	else
 	{
 		temp = *head;
