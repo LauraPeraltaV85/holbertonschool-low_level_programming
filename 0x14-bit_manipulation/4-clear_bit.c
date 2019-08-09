4b48dd2f71affcf68c1159a8c2f37b487732f423
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-09 00:17:26 +0000

    [feature] mask to set the value of a bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *clear_bit - sets value of a bit to 0
+ *@n: pointer to number given
+ *@index: index
+ *Return: 1 if success or -1 if fail
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = ~1 << index;
+
+	if (index < 63)
+	{
+		*n &= mask;
+		return (1);
+	}
+	return (-1);
+}
