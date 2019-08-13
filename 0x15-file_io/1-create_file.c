92e73da0528338112b67cfeca30b929d49f56262
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-08-13 22:00:06 +0000

    [fix] code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_context)
 	if (fd == -1)
 		return (-1);
 
-	if (text_context != NULL)	
+	if (text_context != NULL)
 	{
 		for (ct = 0; text_context[ct] != '\0'; ct++)
 			;
