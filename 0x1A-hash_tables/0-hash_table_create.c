c4ade8165757f214c346de96b25309cef1df0a1b
= <=>
2019-10-11 14:48:38 +0000

    [fix] size declaration

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newhash;
 
-	newhash = malloc(sizeof(hash_table_t));
+	newhash = malloc(sizeof(hash_table_t) * 1);
 	if (newhash == NULL)
 		return (NULL);
 
