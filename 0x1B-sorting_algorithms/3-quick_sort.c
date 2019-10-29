c5d50d6e261c9e70cb2d07934de924710afc7610
= <=>
2019-10-29 15:44:54 +0000

    [feature] quicksort Lomuto scheme

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,29 +1,69 @@
 #include "sort.h"
-
 /**
- *quick_sort - quick sorts an array
- *@array: array to be sorted
- *@size: size of array
+ *partition - Partition for the quicksort using the Lomuto scheme
+ *@array: array to sort
+ *@low: Lowest index of the partition
+ *@high: highest index of the partition
+ *@size: size of the array
+ *Return: position where the partition was made
+ */
+size_t partition(int *array, int low, int high, size_t size)
+{
+	int i, n;
+	int  swap, d;
+
+	d = array[high];
+	i = low - 1;
+	for (n = low; n < high; n++)
+	{
+		if (array[n] < d)
+		{
+			i++;
+			if (i != n)
+			{
+				swap = array[i];
+				array[i] = array[n];
+				array[n] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[high] < array[i + 1])
+	{
+		swap = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = swap;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+/**
+ *quicksort - Sort a partition of an array integer
+ *@array: array to sort.
+ *@low: index of the partition.
+ *@high: highest indes of the partition function sort.
+ *@size: size of the array.
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int d;
+
+	if (low < high)
+	{
+		d = partition(array, low, high, size);
+		quicksort(array, low, d - 1, size);
+		quicksort(array, d + 1, high, size);
+	}
+}
+/**
+ *quick_sort - sort an array of integers.
+ *@array: Array to sort.
+ *@size: Size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
-	int pivot = array[last];
-	int idxs = 0, count;
-
-	if(first < last)
+	if (array != NULL && size > 2)
 	{
-		for(j = idxs + 1; j <= last - 1; j++)
-		{
-			if(array[j] <= pivot)
-			{
-				i++;
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-			}
-		}
-		tmp = array[i + 1];
-		array[i + 1] = array[last];
-		array[last] = tmp;	
-
+		quicksort(array, 0, size - 1, size);
+	}
 }
