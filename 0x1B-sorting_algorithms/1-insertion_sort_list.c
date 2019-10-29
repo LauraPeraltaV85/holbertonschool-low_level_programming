b181e124c167ac58e6525f2134a46d8daf8a0d6e
= <=>
2019-10-29 00:06:18 +0000

    [fix] create

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,28 +1,71 @@
 #include "sort.h"
 
 /**
+ *swap_node - swaps node in list
+ *@left: left node
+ *@right: right node
+ *Return: pointer to list
+ */
+listint_t *swap_node(listint_t *left, listint_t *right)
+{
+	listint_t *head;
+
+	if (left->prev)
+		left->prev->next = right;
+	else
+		head = right;
+
+	if (right->next)
+		right->next->prev = left;
+
+	left->next = rigth->next;
+	right->prev = left->prev;
+	right->next = left;
+	left->prev = right;
+	return (*head);
+}
+
+/**
+ *listint_len - counts list len
+ *@h: list
+ *Return: list len
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
  *insertion_sort_list - sorts list
  *@list: list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *sort = NULL;
+	listint_t *tmp, *sort = NULL;
+	int lenght = listint_len(*list);
 
 	if (!list || !list->next)
 		return;
-		
-	while (list != NULL)
-	{		
-		struct LIST *   pHead  = pList;
-		struct LIST ** ppTrail = &pSorted;
 
-		list = list->next;
+	tmp = *list;
+	while (tmp != NULL)
+	{
 
-		while (!(*ppTrail == NULL || head->n < (*ppTrail)->n)) 
-		{ 
-			ppTrail = &(*ppTrail)->next;
-		}
-		head->next = *ppTrail;
-		*ppTrail = head;
+		while ();
+		{
+			if(!(tmp->prev == NULL || tmp->prev->n < tmp->n))
+			{
+				swap_node(tmp->prev, tmp);
+				print_list(*list);
+				sort = tmp-
+			}
+			tmp = tmp->next;
 		}
 }
